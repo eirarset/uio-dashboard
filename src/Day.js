eb57cce348dfda93e03fd65e95c7f85c72bc52b5
Eirik Årseth <eirik.emil@gmail.com>
2018-09-28 16:42:27 +0200

    Hardcoded courses

--- src/Day.js
+++ src/Day.js
@@ -1,24 +1,31 @@
 import React from 'react'
 import './Day.css'
 
-const Day = () => {
+const Day = (props) => {
+  const lectures = props.lectures.map((element, index) => <Timeslot info={element} key={index} />)
   return (
     <div className='day-card'>
       <div className='day-header-container'>
         <h3>Day</h3>
       </div>
       <div className='time-slots-container'>
-        <Timeslot />
-        <Timeslot />
-        <Timeslot />
+        {lectures}
       </div>
     </div>
   )
 }
 
-const Timeslot = () => {
+const Timeslot = (props) => {
+  const startHours = props.info.startTime.getHours().toString()
+  const startMinutes = (props.info.startTime.getMinutes() < 10) ? ('0' + props.info.startTime.getMinutes().toString()) : props.info.startTime.getMinutes().toString()
+  const endHours = props.info.endTime.getHours().toString()
+  const endMinutes = (props.info.endTime.getMinutes() < 10) ? ('0' + props.info.endTime.getMinutes().toString()) : props.info.endTime.getMinutes().toString()
+  const time = startHours + ':' + startMinutes + '-' + endHours + ':' + endMinutes
   return (
     <div className='time-slot'>
+      <h5>{props.info.course}</h5>
+      {time}
+      <br />
       This is a time slot
     </div>
   )
--- src/Schedule.js
+++ src/Schedule.js
@@ -2,21 +2,87 @@ import React from 'react'
 import './Schedule.css'
 import Day from './Day'
 
-const Schedule = () => {
-  return (
-    <div className='schedule'>
-      <div className='schedule-header-container'>
-        <h1 className='schedule-header'>Schedule</h1>
+class Schedule extends React.Component {
+  state = { courses: [
+    { name: 'IN5140', loaded: false },
+    { name: 'IN5320', loaded: false },
+    { name: 'INF3110', loaded: false },
+    { name: 'IN1900', loaded: false }
+  ] }
+
+  componentDidMount () {
+    // Determin the first and last day of the current week
+    const start = new Date()
+    start.setDate(start.getDate() - start.getDay() + 1)
+    start.setHours(0, 0, 0, 0)
+    const end = new Date(start)
+    end.setDate(end.getDate() + 4)
+    end.setHours(23, 59, 59, 59)
+
+    this.state.courses.forEach(course => {
+      const queryString = `https://data.uio.no/studies/v1/course/${course.name}/semester/18h/schedule?section=FOR`
+      if (!course.loaded) {
+        window.fetch(queryString) // window added to silence eslint
+          .then(res => res.json())
+          .then(result => {
+            const lectures = result.events.filter(lecture => lecture.dtStart && new Date(lecture.dtStart) > start && new Date(lecture.dtStart) < end)
+            const simplifiedLectures = lectures.map(lecture => {
+              return { startTime: new Date(lecture.dtStart), endTime: new Date(lecture.dtEnd) }
+            })
+            this.setState((state, props) => {
+              const newCourses = state.courses.filter(element => element.name !== course.name)
+              newCourses.push({ name: course.name, loaded: true, lectures: simplifiedLectures })
+              return { ...state, courses: newCourses }
+            })
+          },
+          error => {
+            console.log(error)
+          })
+      }
+    })
+  }
+
+  componentDidUpdate () {
+    console.log(this.state)
+  }
+
+  filterByDay = (day) => {
+    const lecturesDay = []
+    this.state.courses.forEach(course => {
+      course.lectures.filter(lecture => lecture.startTime.getDay() === day)
+        .forEach(lecture => lecturesDay.push({ course: course.name, startTime: lecture.startTime, endTime: lecture.endTime }))
+    })
+    return lecturesDay
+  }
+
+  render () {
+    let mondayLectures = []
+    let tuesdayLectures = []
+    let wednesdayLectures = []
+    let thursdayLectures = []
+    let fridayLectures = []
+    if (this.state.courses.every(course => course.loaded)) {
+      mondayLectures = this.filterByDay(1)
+      tuesdayLectures = this.filterByDay(2)
+      wednesdayLectures = this.filterByDay(3)
+      thursdayLectures = this.filterByDay(4)
+      fridayLectures = this.filterByDay(5)
+    }
+    return (
+      <div className='schedule'>
+        <div className='schedule-header-container'>
+          <h1 className='schedule-header'>Schedule</h1>
+        </div>
+        <div className='days-container'>
+          <Day lectures={mondayLectures} />
+          <Day lectures={tuesdayLectures} />
+          <Day lectures={wednesdayLectures} />
+          <Day lectures={thursdayLectures} />
+          <Day lectures={fridayLectures} />
+        </div>
       </div>
-      <div className='days-container'>
-        <Day />
-        <Day />
-        <Day />
-        <Day />
-        <Day />
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Schedule
